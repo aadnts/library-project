1c1aa68a7e5b9ba0c2b6b9d82afec005e18ce217
Antonio.Dantas <antonio.dantas@etu.unige.ch>
2023-09-27 11:47:37 +0200

    feat: login-page UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FieldComponent } from './field/field.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FieldComponent
+    FieldComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
