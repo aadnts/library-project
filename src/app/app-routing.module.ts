a72fbaba820eb22bc21927d9d68435f0693582d9
Antonio.Dantas <antonio.dantas@etu.unige.ch>
2023-10-01 16:58:59 +0200

    feat: documents & home page UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FieldComponent } from './field/field.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DocumentsPageComponent } from './documents-page/documents-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "login", component: LoginPageComponent},
+  {path: "documents", component: DocumentsPageComponent},
+  {path: "home", component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FieldComponent } from './field/field.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DocumentsPageComponent } from './documents-page/documents-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +17,18 @@ import { FooterComponent } from './footer/footer.component';
     LoginComponent,
     FieldComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LoginPageComponent,
+    DocumentsPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'login', component: LoginPageComponent},
+      {path: 'documents', component: DocumentsPageComponent},
+      {path: 'home', component: HomePageComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
